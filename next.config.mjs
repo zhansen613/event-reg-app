63482e28f0b57cc723db893a98c575035da86f5f
zhansen613 <zhansen@eqr.com>
2025-09-10 15:19:13 -0500

    Delete next.config.mjs

--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  // Temporary escape hatch if TS errors block builds:
-  // typescript: { ignoreBuildErrors: true },
-};
-
-export default nextConfig;
