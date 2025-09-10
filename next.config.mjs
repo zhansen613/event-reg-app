781beca3793745a2f9a9926b30d20d097b149828
zhansen613 <zhansen@eqr.com>
2025-09-10 15:19:55 -0500

    Create next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // typescript: { ignoreBuildErrors: true }, // optional temporary escape hatch
+};
+
+export default nextConfig;
